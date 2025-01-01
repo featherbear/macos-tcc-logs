cbab8fb7431d14c016735f8da9436389eceea02d
Andrew Wong <awong6@atlassian.com>
2025-01-01 16:09:45 +1100

    Get identifier for macOS system applications

--- src/main.zig
+++ src/main.zig
@@ -39,7 +39,8 @@ pub fn main() !void {
 
         var evtObject = AppEvent{ .timeString = parsed.value.timestamp, .service = undefined, .bundleId = undefined, .path = null, .outcome = undefined };
 
-        if (std.mem.startsWith(u8, parsed.value.eventMessage, "Handling ")) {
+        try stderr.print("debug {s}\n", .{parsed.value.eventMessage});
+        // debug Handling access request: kTCCServiceFileProviderDomain:<private>:<private>, default_allow: 0, authValue: 1,
 
         if (std.mem.startsWith(u8, parsed.value.eventMessage, "Handling access request to ")) {
             var startIdx: usize = undefined;
@@ -55,7 +56,17 @@ pub fn main() !void {
                 const prefix = "Sub:{";
                 startIdx = std.mem.indexOfPos(u8, parsed.value.eventMessage, endIdx, prefix).? + prefix.len;
                 endIdx = std.mem.indexOfPos(u8, parsed.value.eventMessage, startIdx, "}").?;
-                evtObject.bundleId = parsed.value.eventMessage[startIdx..endIdx];
+                var bundleId = parsed.value.eventMessage[startIdx..endIdx];
+
+                // debug Handling access request to kTCCServiceReminders, from Sub:{/System/Library/PrivateFrameworks/AppleMediaServices.framework/Versions/A/Resources/amsaccountsd}Resp:{TCCDProcess: identifier=com.apple.amsaccountsd, pid=89859, auid=501, euid=501, binary_path=/System/Library/PrivateFrameworks/AppleMediaServices.framework/Versions/A/Resources/amsaccountsd}, ReqResult(Auth Right: Unknown (None), promptType: 1,DB Action:None, UpdateVerifierData)q
+                if (std.mem.indexOf(u8, bundleId, "/") != null) {
+                    const prefix2 = "{TCCDProcess: identifier=";
+                    startIdx = std.mem.indexOfPos(u8, parsed.value.eventMessage, endIdx, prefix2).? + prefix2.len;
+                    endIdx = std.mem.indexOfPos(u8, parsed.value.eventMessage, startIdx, ",").?;
+                    bundleId = parsed.value.eventMessage[startIdx..endIdx];
+                }
+
+                evtObject.bundleId = bundleId;
             }
             {
                 const prefix = ", binary_path=";
